4606f4484cc9a6b328e1c11d545d21dea1ced6f1
ruarai <ruarai.big@gmail.com>
2013-11-05 14:00:11 +1100

    changed download location to download from ruarai.github.io

--- KMPLauncher/UpdateInformationRetriever.cs
+++ KMPLauncher/UpdateInformationRetriever.cs
@@ -12,7 +12,7 @@ namespace KMPLauncher
     static class UpdateInformationRetriever
     {
 
-        static Uri UpdateInfoUri = new Uri("https://gist.github.com/ruarai/7312239/raw/cceba0a2d75a8e14531073295cb0c7e4489f53eb/UpdateInfo");
+        static Uri UpdateInfoUri = new Uri("http://ruarai.github.io/");
 
         public static void Retrieve()
         {
@@ -21,7 +21,7 @@ namespace KMPLauncher
 
             string page = retriever.DownloadString(UpdateInfoUri);
 
-            string[] lines = page.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] lines = page.Split(new string[] { "</br>" }, StringSplitOptions.RemoveEmptyEntries);
 
 
             UpdateInfo.LatestVersion = lines[0];
