067327d50f939420af1271e94e67664cbfa18460
ruarai <ruarai.big@gmail.com>
2013-11-06 17:17:34 +1100

    bugfix

--- KMPLauncher/UpdateInformationRetriever.cs
+++ KMPLauncher/UpdateInformationRetriever.cs
@@ -12,7 +12,7 @@ namespace KMPLauncher
     static class UpdateInformationRetriever
     {
 
-        static const Uri UpdateInfoUri = new Uri("https://dl.dropboxusercontent.com/u/6898485/index.html");//This is where the information for downloading is always stored
+        static Uri UpdateInfoUri = new Uri("https://dl.dropboxusercontent.com/u/6898485/index.html");//This is where the information for downloading is always stored
         //The HTML file itself looks like
         //0.1.2.0
         //http://www.kerbalspaceport.com/wp/wp-content/themes/kerbal/inc/download.php?f=uploads/2013/11/KMP_Client3.zip
